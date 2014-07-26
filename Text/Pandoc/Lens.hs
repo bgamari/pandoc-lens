d44cb32593de412aacebebe089b86b435fa2729c
Ben Gamari <bgamari.foss@gmail.com>
2014-07-25 21:08:48 -0400

    Add Haddocks

--- Text/Pandoc/Lens.hs
+++ Text/Pandoc/Lens.hs
@@ -1,57 +1,82 @@
 {-# LANGUAGE RankNTypes #-}
 
-module Text.Pandoc.Lens where
-                
+module Text.Pandoc.Lens
+    ( -- * Documents
+      body
+      -- * Blocks
+    , _Plain
+    , _Para
+    , _CodeBlock
+    , _BlockQuote
+    , _BulletList
+    , _DefinitionList
+    , _HorizontalRule
+    , _Null
+      -- * Meta values
+    , meta
+      -- * Attributes
+    , HasAttr(attributes)
+    ) where
+
 import Control.Applicative
 import Control.Lens
 import Text.Pandoc.Definition
 import Data.Map (Map)
 
+-- | The body of a pandoc document
 body :: Lens' Pandoc [Block]
 body = lens (\(Pandoc _ b)->b) (\(Pandoc m _) b->Pandoc m b)
 
+-- | A prism on a plain block
 _Plain :: Prism' Block [Inline]
 _Plain = prism' Plain f
   where
     f (Plain x) = Just x
     f _         = Nothing
 
+-- | A prism on a paragraph block
 _Para :: Prism' Block [Inline]
 _Para = prism' Para f
   where
     f (Para x)  = Just x
     f _         = Nothing
 
+-- | A prism on the text of a code block
 _CodeBlock :: Prism' Block String
 _CodeBlock = prism' (CodeBlock nullAttr) f
   where
     f (CodeBlock _ x)    = Just x
     f _                  = Nothing
 
+-- | A prism on a block quote
 _BlockQuote :: Prism' Block [Block]
 _BlockQuote = prism' BlockQuote f
   where
     f (BlockQuote x)     = Just x
     f _                  = Nothing
 
+-- | A prism on the items of a bullet list block
 _BulletList :: Prism' Block [[Block]]
 _BulletList = prism' BulletList f
   where
     f (BulletList x)     = Just x
     f _                  = Nothing
-      
+
+-- | A prism on the items of a definition list
 _DefinitionList :: Prism' Block [([Inline], [[Block]])]
 _DefinitionList = prism' DefinitionList f
   where
     f (DefinitionList x) = Just x
     f _                  = Nothing
 
+-- | A prism on a horizontal rule
 _HorizontalRule :: Prism' Block ()
 _HorizontalRule = prism' (const HorizontalRule) f
   where
     f HorizontalRule     = Just ()
     f _                  = Nothing
 
+-- | A prism on a null block
 _Null :: Prism' Block ()
 _Null = prism' (const Null) f
   where
